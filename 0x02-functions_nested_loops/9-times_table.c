b24aff4189a47b171f1aabfc3d21e9f46db8209f
oghenetega16 <sukuruoghenetega@gmail.com>
2022-09-13 21:50:26 -0700

    Done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i =0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -34,5 +34,5 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-}
+i}
 
