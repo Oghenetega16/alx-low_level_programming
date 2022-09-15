23747fd7ec11bb3c3c36243ad60187ec36351df9
oghenetega16 <sukuruoghenetega@gmail.com>
2022-09-14 20:47:49 -0700

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0')
+					_putchar(((j * i) % 10) + '0');
 				}
 
 				else if ((i * j) < 1000)
