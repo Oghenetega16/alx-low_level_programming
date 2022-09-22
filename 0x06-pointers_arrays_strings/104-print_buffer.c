7e1cbc7731d9dff8d0969b97cc9df8205f41dd3a
oghenetega16 <sukuruoghenetega@gmail.com>
2022-09-22 07:07:14 -0700

    done

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,31 +2,42 @@
 #include <stdio.h>
 
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l line of buffer to print
- *
- * Return: void
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: size of buffer
+ * Return: no return
  */
-
-void print_line(char *c, int s, int l)
+void print_buffer(char *b, int size)
 {
-	int j, k;
-	for (j = 0; j <= 9; j++)
+	int j, k, l;
+
+	if (size <= 0)
+		printf("\n");
+	else
 	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf(" ");
+			}
 			printf(" ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else 
-			putchar(',');
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
 	}
 }
