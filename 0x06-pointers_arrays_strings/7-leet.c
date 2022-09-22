5ff4709670797234f9a82efff2139d353eda715f
oghenetega16 <sukuruoghenetega@gmail.com>
2022-09-22 05:52:23 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,10 +3,10 @@
 /**
  * leet - encodes a string into 1337
  * letters a and A are replaced by 4
- * letters e and E are replaced by 3
- * letters o and O are replaced by 0
- * letters t and T are replaced by 7
- * letters l and L are replaced by 1
+ * Letters e and E are replaced by 3
+ * Letters o and O are replaced by 0
+ * Letters t and T are replaced by 7
+ * Letters l and L are replaced by 1
  * @s: pointer to string
  *
  * Return: pointer to s
@@ -22,7 +22,7 @@ char *leet(char *s)
 	while (s[stringCount] != '\0')
 		/* check whether leetLetter is found */
 	{
-		leetCount =0;
+		leetCount = 0;
 		while (leetCount < 10)
 		{
 			if (leetLetters[leetCount] == s [stringCount])
