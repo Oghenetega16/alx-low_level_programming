f6027e94d49881177513e2bcb888e62d7203a5b4
oghenetega16 <sukuruoghenetega@gmail.com>
2022-09-28 19:27:54 -0700

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -34,7 +34,7 @@ int comparator(char *s, int n1, int n2)
 /**
  * is_palindrome - detects if a string is a palindrome
  * @s: string
- * Return: 1 if s is a palindrome, 0 if not
+ * Return: 1 if it is a palindrome, 0 if not
  */
 int is_palindrome(char *s)
 {
