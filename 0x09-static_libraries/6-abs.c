28987860a3d71202b37c5686574de3dcc5e355c9
oghenetega16 <sukuruoghenetega@gmail.com>
2022-10-01 21:23:56 -0700

    done

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - check main
+ * @i: an integer input
+ *
+ * description: returns absolute value of an integer
+ * Return: absolute value of integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
