5ac6e751ec6cc64dcd5e82783cef05bc848d36d5
oghenetega16 <sukuruoghenetega@gmail.com>
2022-10-11 03:44:04 -0700

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,10 +23,10 @@ int _strlen(char *s)
 
 /**
  * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
+ * including the terminating null byte
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
+ * @src: sting to be copied
  *
  * Return: the pointer to dest
  */
@@ -54,9 +54,9 @@ char *_strcpy(char *dest, char *src)
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
- * @owner; owner of the dog
+ * @owner: owner of the dog
  *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -89,5 +89,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (dog);
 }
-
-
